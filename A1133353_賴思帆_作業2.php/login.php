e5898ba0276b7c7cb4a2a91a6ef23cf333b295c4
fran <a1133353@mail.nuk.edu.tw>
2026-04-09 20:51:11 +0800

    Add files via upload

--- A1133353_賴思帆_作業2.php/login.php
+++ A1133353_賴思帆_作業2.php/login.php
@@ -1,1 +1,20 @@
+<html>
+<head>
+    <title>登入帳號</title>
+</head>
 
+<body>
+<center>
+<h2>夏令營系統登入</h2>
+
+<form action="logincheck.php" method="POST">
+    帳號：<input type="text" name="uID"><br/><br/>
+    密碼：<input type="password" name="uPWD"><br/><br/>
+
+    <input type="submit" value="登入">
+</form>
+
+</center>
+
+</body>
+</html>
