ed8a143b8181b071daa33a4fc2c77d8412ecb913
fran <a1133353@mail.nuk.edu.tw>
2026-04-16 22:54:48 +0800

    Update login.php

--- A1133353_賴思帆_作業3/login.php
+++ A1133353_賴思帆_作業3/login.php
@@ -1,1 +1,17 @@
+<?php
 
+if(isset($_COOKIE['nName'])){
+    echo $_COOKIE['nName']."歡迎回來";
+    echo "<a href='cookiedel.php'>刪除cookie</a>";
+}
+?>
+<form action="logincheck.php" method="POST">
+    帳號：<input type="text" name="uID"><br/><br/>
+    密碼：<input type="password" name="uPWD"><br/><br/>
+
+    <input type="submit" value="登入">
+</form>
+
+<?php
+echo time();
+?>
